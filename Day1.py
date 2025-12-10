5b6d35621ea1d227ca7531fdb2ad0275a9ba09e2
PrathikshaSathish26 <prathikshasathish.2611@gmail.com>
2025-12-10 09:41:57 +0530

    i

--- Day1.py
+++ Day1.py
@@ -29,7 +29,69 @@
 #     sum+=i
 # print(sum)
 
-#reverse of a number
+#5.greatest of two numbers
+# n=int(input())
+# n2=int(input())
+# if n>n2:
+#     print(n)
+# elif n2>n:
+#     print(n2)
+# else:
+#     print("same")
+
+#6.greatest of three numbers
+# n1=int(input())
+# n2=int(input())
+# n3=int(input())
+# if n1>n2 & n1>n3:
+#     print(n1)
+# elif n2>n1 & n2>n3:
+#     print(n2)
+# else:
+#     print(n3)
+
+#7.Leapyear or not
+# n=int(input())
+# if n%400:
+#     if n%4 & n%100!=0:
+#         print("y")
+# else:
+#     print("n")
+
+#8. prime number
+# n=int(input())
+# c=0
+# for i in range(2,n):
+#     if n%i==0:
+#         c+=1
+# if c>0:
+#     print("not prime")
+# else:
+#     print("prime")
+
+
+#9. prime number in given range
+# start = int(input())
+# end = int(input())
+
+# for num in range(start, end + 1):
+#     if num > 1:
+#         for i in range(2, num):
+#             if num % i == 0:
+#                 break
+#         else:
+#             print(num)
+
+#10. sum of digits of a number
+n=int(input())
+sum=0
+while n>0:
+    d=n%10
+    sum+=d
+    n//=10
+print(sum)
+
+
 
 
 
