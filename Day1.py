54e1d8dc6f91a854a673bb766d78ac4d50e75365
PrathikshaSathish26 <prathikshasathish.2611@gmail.com>
2025-12-10 09:13:58 +0530

    i

--- Day1.py
+++ Day1.py
@@ -1,1 +1,54 @@
-n=int(input())
+#1. positive negative
+#n=int(input())
+#if n>0:
+ #   print("positive")
+#elif n<0:
+ #   print("negative")
+#else:
+ #   print("zero")
+
+#2.Even or odd
+#n=int(input())
+#if n%2==0:
+ #   print("e")
+#else:
+ #   print("o")
+
+# #3.Sum of n natural no
+# n=int(input())
+# sum=0
+# for i in range(1,n):
+#     sum+=i
+# print(sum)
+
+# #4. range
+# l=int(input())
+# h=int(input())
+#sum=0
+# for i in range(l,h):
+#     sum+=i
+# print(sum)
+
+#reverse of a number
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
